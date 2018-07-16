9d1adf139714c50f5e2a43d48d7dbd8e1ccbbdfc
nsis <grand-roman@yandex.ru>
2018-07-16 13:22:51 +0700

    compliebt forgetkogin

---
+++ client/src/view/ForgetLoginScreen.hx
@@ -0,0 +1,49 @@
+package view;
+
+import router.RouterLocation.RouterAction;
+import action.ScholaeAction;
+import react.ReactComponent.ReactElement;
+import redux.react.IConnectedComponent;
+import view.LoginView;
+import router.RouteComponentProps;
+import react.ReactComponent.ReactComponentOfPropsAndState;
+import react.ReactMacro.jsx;
+
+import utils.TimerHelper.defer;
+
+class LoginScreen
+    extends ReactComponentOfPropsAndState<RouteComponentProps, LoginViewProps>
+    implements IConnectedComponent {
+
+    public function new(props:RouteComponentProps) {
+        super(props);
+    }
+
+    public override function render(): ReactElement {
+        return jsx('<LoginView {...state} dispatch=$dispatch/>');
+    }
+
+    function mapState(state: ApplicationState, props: RouteComponentProps): LoginViewProps {
+
+            if (state.scholae != null && state.scholae.auth.loggedIn && state.scholae.auth.returnPath != null) {
+                var returnPath = state.scholae.auth.returnPath;
+                defer(function() {
+                    props.router.replace(
+                        {
+                            pathname: returnPath,
+                            search: null,
+                            state: null,
+                            action: RouterAction.REPLACE,
+                        });
+                    dispatch(ScholaeAction.PreventLoginRedirection);
+                 });
+            }
+
+        return
+            {
+                signIn: function(email, password) {
+                    dispatch(ScholaeAction.Authenticate(email, password));
+                }
+            };
+    }
+}
