2a6c539ef6f2712757040029ea73e76da64aeaca
alina <alya.otyrgasheva@mail.ru>
2018-11-26 12:44:22 +0700

    issue59 -only start

--- client/src/view/teacher/TeacherDashboardView.hx
+++ client/src/view/teacher/TeacherDashboardView.hx
@@ -17,7 +17,8 @@ typedef TeacherDashboardProps = {
     showNewGroupView: Bool,
     lastAttempts: Array<AttemptMessage>
 }
-
+//при удалении курса меняется статус на тру, если deleted == false то все показывается, в остальных связанных таблицах (groups,assignments,trainings,exercises),
+// также надо просмотреть те места, где будет проверка на наличие данного курса и заданий, у ученика и учителя
 class TeacherDashboardView extends ReactComponentOfProps<TeacherDashboardProps> implements IConnectedComponent {
 
     public function new()
@@ -26,7 +27,7 @@ class TeacherDashboardView extends ReactComponentOfProps<TeacherDashboardProps> 
     }
 
     override function render() {
-        var list = [ for (g in props.groups) if (props.group.deleted != true) jsx('<div className="uk-margin" key=${g.id}><span data-uk-icon="users"></span> <Link className="uk-margin-small-left" to=${"/teacher/group/" + g.id}>${g.name}</Link></div>') ];
+        var list = [ for (g in props.groups) if (g.deleted != true) jsx('<div className="uk-margin" key=${g.id}><span data-uk-icon="users"></span> <Link className="uk-margin-small-left" to=${"/teacher/group/" + g.id}>${g.name}</Link></div>') ];
         var newGroup =
             if (props.showNewGroupView)
                 jsx('<NewGroupView dispatch=${dispatch} close=$onCloseAddGroupClick/>')
