f75eec8597cb61e8d04de31a89116a3eef725a1e
lipgen <leonid.tumoyakov@gmail.com>
2019-02-01 12:45:17 +0700

    fixing `MessageQueue` set starting process throw enviroment variables on windows system

--- server-shared/MessageQueue.hx
+++ server-shared/MessageQueue.hx
@@ -5,11 +5,16 @@ import haxe.io.Bytes;
 
 class MessageQueue {
     public static function publish(routingKey: String, exchange: String, payload: Bytes, vhost: String = "/") {
-        var args = ["--user=scholae", "--password=scholae", "--vhost="+vhost, "publish", "routing_key="+routingKey, "exchange="+exchange];
-        var p = new Process("/usr/local/sbin/rabbitmqadmin", args);
+        var rabbit_path_args = Sys.getEnv("SCHOLAE_RABBIT_PATH");
+        var processName = if(rabbit_path_args != null) "python.exe"
+            else "/usr/local/sbin/rabbitmqadmin";
+        trace(processName+": "+rabbit_path_args);
+        var args = [rabbit_path_args,"--user=scholae", "--password=scholae", "--vhost="+vhost, "publish", "routing_key="+routingKey, "exchange="+exchange];
+        var p = new Process(processName, args);
         p.stdin.write(payload);
         p.stdin.close();
-        p.exitCode(true);
+        if(p.exitCode(true) != 0) {
+            trace(p.stderr.readAll().toString());
+        }
     }
-
 }
