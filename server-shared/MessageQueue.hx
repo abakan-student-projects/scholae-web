06c0ca40311883301e9fb844d5c1bbf378c683f4
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2019-02-07 09:06:04 +0700

    - check for nullness; - add worker to the main build.

--- server-shared/MessageQueue.hx
+++ server-shared/MessageQueue.hx
@@ -6,10 +6,11 @@ import haxe.io.Bytes;
 class MessageQueue {
     public static function publish(routingKey: String, exchange: String, payload: Bytes, vhost: String = "/") {
         var scholaeRabbitPath = Sys.getEnv("SCHOLAE_RABBIT_PATH");
-        var envArgs: Array<String> = scholaeRabbitPath.split(" : ");
+        var envArgs: Array<String> = if(null != scholaeRabbitPath) scholaeRabbitPath.split(" : ") else [];
         var processName = if(scholaeRabbitPath != null) envArgs.shift() else "/usr/local/sbin/rabbitmqadmin";
-        var args = ["--user=scholae", "--password=scholae", "--vhost="+vhost, "publish", "routing_key="+routingKey, "exchange="+exchange];
-        var p = new Process(processName, envArgs.concat(args));
+        var args = envArgs.concat(["--user=scholae", "--password=scholae", "--vhost="+vhost, "publish", "routing_key="+routingKey, "exchange="+exchange]);
+        trace(processName + " " + args);
+        var p = new Process(processName, args);
         p.stdin.write(payload);
         p.stdin.close();
         if(p.exitCode(true) != 0) {
